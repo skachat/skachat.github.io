8ad6ad0d73eb0048f4b598cba54ad5c49d655a00
dev <user@users.noreply.github.com>
2020-11-10 21:38:13 +0300

    63

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Скачать PhoenixMiner 5.1c AMD/NVIDIA Ethereum [2020]",
+        "excerpt":"PhoenixMiner 5.1c - стабильная версия, решающая ошибки 4 ГБ видеокарт в Windows 10. Скачать и настроить программу Требования PhoenixMiner Майнер работает с двумя основными брендами видеокарт. AMD - Vega, 580, 570, 560, 480, 470, 460, 390, 290 и более старые модели с достаточным количеством VRAM. Nvidia - 2080 ti, 2080,...","categories": ["Miners"],
+        "tags": [],
+        "url": "https://skachat.cc/miners/phoenix-miner-5.1c/",
+        "teaser": "https://skachat.cc/assets/images/thumbs/500x300.png"
+      },]
